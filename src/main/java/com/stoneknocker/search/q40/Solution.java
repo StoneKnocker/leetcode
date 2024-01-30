4c2fbd3755c059599dd5f38b054cca969f34e548
xianyang <xianyang.lu@vcg.com>
2024-01-30 16:17:25 +0800

    q40

--- src/main/java/com/stoneknocker/search/q40/Solution.java
+++ src/main/java/com/stoneknocker/search/q40/Solution.java
@@ -1,10 +1,12 @@
-package com.stoneknocker.search.q39;
+package com.stoneknocker.search.q40;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
         List<List<Integer>> results = new ArrayList<>();
         combination(candidates, 0, target, new ArrayList<>(), results);
         return results;
@@ -17,8 +19,9 @@ public class Solution {
         } else {
             for (int i=startIndex; i<candidates.length; i++) {
                 if (target - candidates[i] >= 0) {
+                    if (i>startIndex && candidates[i]==candidates[i-1]) continue;
                     components.add(candidates[i]);
-                    combination(candidates, i, target - candidates[i], components, results);
+                    combination(candidates, i+1, target - candidates[i], components, results);
                     components.remove(components.size()-1);
                 }
             }
@@ -27,8 +30,11 @@ public class Solution {
 
     public static void main(String[] args) {
         var solution = new Solution();
-        System.out.println(solution.combinationSum(new int[]{2,3,5}, 8));
-        System.out.println(solution.combinationSum(new int[]{2,3,6,7}, 7));
-        System.out.println(solution.combinationSum(new int[]{2}, 1));
+        System.out.println(solution.combinationSum2(new int[]{2,3,5}, 8));
+        System.out.println(solution.combinationSum2(new int[]{2,3,6,7}, 7));
+        System.out.println(solution.combinationSum2(new int[]{2}, 1));
+        System.out.println(solution.combinationSum2(new int[]{10,1,2,7,6,1,5}, 8));
+        System.out.println(solution.combinationSum2(new int[]{1,1,1,2,3,}, 3));
+        System.out.println(solution.combinationSum2(new int[]{1,1,1,2,3,}, 4));
     }
 }
