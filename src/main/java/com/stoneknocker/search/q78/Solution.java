1083de21ff0a45fc4dab0fa1aa6505088826c92e
xianyang <xianyang.lu@vcg.com>
2024-01-31 10:51:05 +0800

    q78

---
+++ src/main/java/com/stoneknocker/search/q78/Solution.java
@@ -0,0 +1,36 @@
+package com.stoneknocker.search.q78;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> combination = new ArrayList<>();
+        for (int i=0; i<=nums.length; i++) {
+            findSubset(result, combination, nums, i, i, 0);
+        }
+
+        return result;
+    }
+
+    void findSubset(List<List<Integer>> result, List<Integer> combination, int[] nums, int combinationNum, int absentNum, int startPos) {
+        if (absentNum == 0) {
+            result.add(new ArrayList<>(combination));
+            return;
+        }
+        if (startPos >= nums.length) {
+            return;
+        }
+        combination.add(nums[startPos]);
+        findSubset(result, combination, nums, combinationNum, absentNum-1, startPos+1);
+        combination.remove(combination.size()-1);
+        findSubset(result, combination, nums, combinationNum, absentNum, startPos+1);
+    }
+
+    public static void main(String[] args) {
+        var solution = new Solution();
+        System.out.println(solution.subsets(new int[]{1,2,3}));
+        System.out.println(solution.subsets(new int[]{0}));
+    }
+}
