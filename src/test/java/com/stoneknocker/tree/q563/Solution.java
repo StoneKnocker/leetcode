ed88501811c3ae624d6b9ec48460de835d8bb4f0
xianyang <xianyang.lu@vcg.com>
2024-03-20 15:35:44 +0800

    q563 tree

---
+++ src/test/java/com/stoneknocker/tree/q563/Solution.java
@@ -0,0 +1,23 @@
+package com.stoneknocker.tree.q563;
+
+import com.stoneknocker.tree.TreeNode;
+import java.util.List;
+
+public class Solution {
+    private int tiltSum;
+    public int findTilt(TreeNode root) {
+        tiltSum = 0;
+        travel(root);
+        return tiltSum;
+    }
+
+    private int travel(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        var leftValue =  travel(node.left);
+        var rightValue =  travel(node.right);
+        tiltSum += Math.abs(leftValue - rightValue);
+        return leftValue + rightValue;
+    }
+}
